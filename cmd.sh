4b73407a372a87b53b8ba3c9ebb3f3c33ba2a330
Habibun Noby <habibun.noby@gmail.com>
2022-06-22 05:36:14 +0600

    3-zero

---
+++ cmd.sh
@@ -0,0 +1,10 @@
+symfony new guestbook --version=5.4 --php=8.1 --webapp --docker --cloud
+mkdir public/images/
+php -r "copy('http://clipartmag.com/images/website-under-construction-image-6.gif', 'public/images/under-construction.gif');"
+symfony server:start -d
+symfony open:local
+php -r "copy('https://symfony.com/favicon.ico', 'public/favicon.ico');"
+symfony cloud:project:create --title="Guestbook" --plan=development
+symfony cloud:deploy
+symfony cloud:url -1
+symfony cloud:project:delete
