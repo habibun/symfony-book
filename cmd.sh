0f3964a67d57909290275586032e077f8c40c09f
Habibun Noby <habibun.noby@gmail.com>
2022-06-22 06:45:34 +0600

    7-database

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,11 @@
+symfony server:ca:install
+symfony book:check-requirements
+symfony new --version=5.4-1 --book guestbook
+symfony book:checkout 10
+symfony book:checkout 10.2
+git diff step-10-1...step-10-2
+git diff step-9...step-10-1
+git log -- src/Controller/ConferenceController.php
 symfony new guestbook --version=5.4 --php=8.1 --webapp --docker --cloud
 mkdir public/images/
 php -r "copy('http://clipartmag.com/images/website-under-construction-image-6.gif', 'public/images/under-construction.gif');"
@@ -13,3 +21,15 @@ export APP_ENV=dev
 symfony cloud:logs --tail
 symfony cloud:ssh
 symfony console list make
+docker-compose up -d
+docker-compose ps
+docker-compose logs
+symfony run psql
+docker-compose exec database psql app symfony
+symfony run pg_dump --data-only > dump.sql
+symfony run psql < dump.sql
+symfony cloud:tunnel:open
+symfony var:expose-from-tunnel
+symfony cloud:tunnel:close
+
+
